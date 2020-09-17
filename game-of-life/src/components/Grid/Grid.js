bae95a34de7f1e481c6b3e3fca626e9f84600621
Carlos Banks <cbanks@clypr.co>
2020-09-16 21:05:00 -0400

    rendering on screen

---
+++ game-of-life/src/components/Grid/Grid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Grid = () => {
+
+        return (
+            <div className="grid-container">
+                <p>GRID COMPONENT</p>
+            </div>
+        )
+}
+
+
+
+export default Grid; 
---
+++ game-of-life/src/components/GridContainer/GridContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GridContainer = () => {
+
+        return (
+            <div className="grid-container">
+                <p>GRID CONTAINER COMPONENT</p>
+            </div>
+        )
+}
+
+
+
+export default GridContainer; 
